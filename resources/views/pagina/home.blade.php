71507ff9654bae7012fdec8702d1854377141d80
AndreaNova1 <69744573+AndreaNova1@users.noreply.github.com>
2022-06-02 00:11:15 -0600

    Se modificaron las vistas, se crearon rutas.

---
+++ resources/views/pagina/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layoust.base')
+
+@section('title', 'Login')
+
+@section('content')
+<body>
+
+<div class="container mt-5"> @yield('content')
+    <center>
+        <a class=" container  navbar-center" href="#">
+        </a>
+        <h1>PROYECTO DE SISTEMAS OPERATIVOS II</h1>
+        <h1>GRUPO II</h1>
+    </center>
+</div>
+</body>
+</html>
+@endsection
